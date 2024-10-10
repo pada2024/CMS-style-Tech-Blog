f4e83903dd5cbc811d93269e011ef48067ce1969
Parisa Daily <misssweetardor@yahoo.com>
2024-10-09 19:01:06 -0700

    slightly modifiied index.js for testing purpsoes

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
+// const express = require('express');
+// const session = require('express-session');
+// const routes = require('./controllers');
+
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
+
+// app.use(session(sess));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
@@ -19,6 +50,15 @@ const sess = {
 };
 
 app.use(session(sess));
+
+// Set Content Security Policy headers to allow font loading
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;"
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
